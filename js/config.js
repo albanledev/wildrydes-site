a91a69bbf5b178dd0403c9c75fca90b63753f828
Alban de Braquilanges <alban.debraquilanges@canal-plus.com>
2025-04-21 21:07:18 +0200

    ajout cognito credentials

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_ssBi9RVD1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5cbldki4d3jgauv1u4953b91jp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4laf9g21ujpv3kkli1rfmfhe3a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
